69cc922e84bf66f5a9c9a5c0a2f6cd4128b8cf43
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-04-06 17:18:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write your code here
+const LIST_URL
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+const DETAILS_URL
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+
+function getPhones(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          return Promise.reject(new Error(
+            `${response.status} - ${response.statusText}`
+          ));
+        }
+
+        return response.json();
+      })
+      .then(result => {
+        return resolve(result);
+      });
+
+    setTimeout(() => {
+      reject(new Error('Error'));
+    }, 5000);
+  });
+}
+
+function getPhonesDetails(ids) {
+  const phonesDetails = ids.map(id => (
+    fetch(`${DETAILS_URL}${id}.json`)
+  ));
+
+  return Promise.all(phonesDetails);
+}
+
+getPhones(LIST_URL)
+  .then(phones => {
+    const listOfPhoneName = document.createElement('ul');
+
+    phones.forEach(phone => {
+      const itemOfPhoneName = document.createElement('li');
+
+      itemOfPhoneName.textContent = phone.name;
+      listOfPhoneName.append(itemOfPhoneName);
+    });
+
+    document.body.append(listOfPhoneName);
+  })
+  .then(result => {
+    return getPhonesDetails(result);
+  });
